c84aebf76fc455429ee14f4b81ce3ff796a57581
nikitazaginey <nikitazaginey1@gmail.com>
2022-07-14 13:13:26 +0200

    add class FactorialFind with static method Find

--- FactorialLinq/Program.cs
+++ FactorialLinq/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace FactorialLinq
+﻿using System.Numerics;
+
+namespace FactorialLinq
 {
+    // I use BigInteger, because if you take the factorial of a large number, there will not be enough values ​​in int32
+    public class FactorialFind
+    {
+        public static BigInteger Find(int range)
+        {
+           var numbers = Enumerable.Range(1, range);
+           return numbers.Aggregate(BigInteger.One, (a, b) => a * b);
+     
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            
         }
     }
 }
